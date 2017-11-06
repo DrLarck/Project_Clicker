0ada7079cf6a3feb21188b6eb9f60176a491b6b6
DrLarck <33419226+DrLarck@users.noreply.github.com>
2017-11-06 13:20:55 +0100

    Create constante.h

---
+++ constante.h
@@ -0,0 +1,15 @@
+#ifndef CONSTANTE_H_INCLUDED
+#define CONSTANTE_H_INCLUDED
+
+    /* Fenetre */
+
+    #define LARGEUR_WINDOW      700
+    #define HAUTEUR_WINDOW      600
+
+    /* Boutons */
+
+    #define BOUTON_RELACHE      0
+    #define BOUTON_APPUYE       1
+
+
+#endif // CONSTANTE_H_INCLUDED
